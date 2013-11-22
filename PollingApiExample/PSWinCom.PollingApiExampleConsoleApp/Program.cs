103948e8fe0a39ea280d4be6785fecd73a125a63
Bjørn Otto Vasbotten <bjorn@pswin.com>
2013-11-22 15:47:33 +0100

    Retrieve SMS and Delivery reports from the PSWinCom Gateway using the SOAP Polling API https://wiki.pswin.com/POLLING.ashx

---
+++ PollingApiExample/PSWinCom.PollingApiExampleConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using log4net;
+using PSWinCom.PollingApiExampleConsoleApp.PollingApi;
+
+namespace PSWinCom.PollingApiExampleConsoleApp
+{
+    class Program
+    {
+        private static ILog log = LogManager.GetLogger("Receiver");
+
+        static void Main(string[] args)
+        {
+            log.Info("Starting sample app");
+            PollSoapClient client = new PollSoapClient();
+
+            // Run the sample for some minutes, normally we would have a windows service running
+            for (int i = 0; i < 60; i++)
+            {                
+                var fetchResponse = client.FetchNextMessage("USERNAME", "PASSWORD", true);
+
+                if (fetchResponse.StatusCode == 200)
+                {
+                    log.InfoFormat("Service found message with id: {0}, which must be acked.", fetchResponse.MessageID);
+                    
+                    var ackResult = client.AcknowledgeMessage("USERNAME", "PASSWORD", fetchResponse.MessageID);
+
+                    if (ackResult.StatusCode == 210)
+                    {
+                        log.InfoFormat("Message was successfully acked, at this we can safely store retrieved message");
+                        var message = fetchResponse.SMSMessage;
+                        log.InfoFormat("SMS from {0} to {1} with text: {2}", message.SenderNumber, message.SenderNumber, message.Text);
+                    }
+                    else
+                        log.Error("Failed to ack message!");
+                }
+                else            
+                    log.WarnFormat("Service responded {0}, indicating no message can be retrieved.", fetchResponse.StatusCode);
+                             
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
---
+++ PollingApiExample/README.md
@@ -0,0 +1,6 @@
+PSWinCom-Gateway-PollingApiExample
+==================================
+
+.Net C# Console application which demonstrates retrieving MO (Mobile Originated) SMS and DR (Delivery Reports) from the PSWinCom Gateway Polling API
+
+API Documentation is found on the PSWinCom Wiki: https://wiki.pswin.com/POLLING.ashx
