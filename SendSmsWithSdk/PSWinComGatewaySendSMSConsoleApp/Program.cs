424c71764adb147304d547d2c5a5f6c12ce47906
Bjørn Otto Vasbotten <bjorn@pswin.com>
2013-11-22 15:46:02 +0100

    Send SMS using the PSWinCom Gateway .Net SDK https://wiki.pswin.com/Developer-Tools.ashx#NET_0

---
+++ SendSmsWithSdk/PSWinComGatewaySendSMSConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using PSWinCom.Gateway.Client;
+
+namespace PSWinComGatewaySendSMSConsoleApp
+{
+    class Program
+    {
+        private static ILog log = LogManager.GetLogger("Default");
+
+
+        static void Main(string[] args)
+        {
+            log.Info("Sending message");
+
+            try
+            {
+                var client = new SMSClient();
+                client.Username = "username";
+                client.Password = "password";
+
+                var message = new Message();
+                message.ReceiverNumber = "4799495153";
+                message.SenderNumber = "PSWinCom";
+                message.Text = "PSWinCom Gateway sample message";
+
+                client.Messages.Add(0, message);
+                client.SendMessages();
+
+                log.Info("Sending message finished");
+            }
+            catch (Exception e)
+            {
+                log.Error("Exception when sending SMS", e);
+            }
+        }
+    }
+}
